1842bfaec31daece621d3b4273687250762c6741
Atul Sharma <atuljisharma7@gnail.com>
2022-03-28 16:44:58 +0530

    Auth guard NA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ChildComponent } from './child/child.component';
+import { AuthGuard } from './shared/auth.guard';
 const ROUTES: Route[] = [
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'home/:username', component: HomeComponent},
    {path: 'home/:username/:id/:name/:email/:phone/:website/:street/:suite/:city/:zipcode/:company', component: ChildComponent,
-  },
+   canActivate:[AuthGuard]  },
 ]
 @NgModule({
   imports: [
